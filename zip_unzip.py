a535f94aefe57e0b48566210045324052f453904
omvikram <omvikram@hotmail.com>
2020-08-01 22:15:29 +0530

    zip unzip

---
+++ zip_unzip.py
@@ -0,0 +1,25 @@
+import zipfile
+import os
+import pdb
+
+path = os.getcwd()
+path = path + "/myzip/"
+#os.mkdir(path + "/myzip")
+
+# f1 = open(path + "file1.txt","w")
+# f1.write("I am file1.txt")
+# f1.close()
+
+#pdb.set_trace()
+
+# f2 = open(path + "file2.txt","w")
+# f2.write("I am file2.txt")
+# f2.close()
+
+# How to compress the files into zip folder 
+# zz = zipfile.ZipFile(path+"myzip.zip", "w")
+# zz.write(path+"file1.txt", compress_type=zipfile.ZIP_DEFLATED)
+# zz.write(path+"file2.txt", compress_type=zipfile.ZIP_DEFLATED)
+# zz.close()
+
+
