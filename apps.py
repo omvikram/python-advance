7f0fc2dd9c3a6aa475fb0359119279820977134e
omvikram <omvikram@hotmail.com>
2023-06-26 05:17:34 +0000

    merging changes

--- apps.py
+++ apps.py
@@ -1,1 +1,27 @@
 ##bootcamp file for practice
+
+def move_zeros_to_left(arr):
+    if len(arr) < 1:
+        return arr
+        
+    tempArr = []
+    readIndex = 0
+    
+    for i in range(0, len(arr)):
+        if(arr[i] == 0):
+            tempArr.append(i)
+            
+    for j in range(0, len(tempArr)):
+        index = tempArr[j]
+        print(index)
+        arr.pop(index)
+        arr.insert(0,0)
+
+v = [1, 10, 20, 0, 59, 63, 0, 88, 0]
+print("Original Array:", v)
+
+move_zeros_to_left(v)
+
+print("After Moving Zeroes to Left: ", v)
+
+
