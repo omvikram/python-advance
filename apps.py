1c600e1caccf4a40d1325cd6e7baf8eef907ed95
omvikram <omvikram@hotmail.com>
2023-06-26 05:35:46 +0000

    apps & apps1

--- apps.py
+++ apps.py
@@ -1,5 +1,7 @@
 ##bootcamp file for practice
-
+## Given an integer array, move all elements that are 0 to the left while 
+## maintaining the order of other elements in the array. 
+## The array has to be modified in-place.
 def move_zeros_to_left(arr):
     if len(arr) < 1:
         return arr
